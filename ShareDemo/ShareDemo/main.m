674e7550c435d2780a0098bad8e0c2773061aaa6
Jerry（韩伟鹏） <hanweipeng@wilddog.com>
2018-04-25 17:51:03 +0800

    123

--- ShareDemo/ShareDemo/main.m
+++ ShareDemo/ShareDemo/main.m
@@ -12,5 +12,5 @@
 int main(int argc, char * argv[]) {
     @autoreleasepool {//master
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    }    
 }
